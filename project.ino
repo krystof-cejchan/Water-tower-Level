a7e4bae88bdde4b3dfab5aaffd2eda18138aac17
Kryštof Čejchan <krystofcejchan@seznam.cz>
2025-04-23 11:10:59 +0200

    Update project.ino

--- project.ino
+++ project.ino
@@ -23,28 +23,26 @@ void loop() {
   ps.update();
   double dist = distance();
   lcd.clear();
-  lcdPrint(0, 1, dist + " cm");
-
+  String SerialData = "";
+  SerialData = String(dist, 2);
+  lcd.setCursor(0, 1);
+  lcd.print(SerialData + " cm");
   if (dist > distLimitCM) {
     //hladina je příliš daleko
     tone(buzzerPin, 1000);
-    lcdPrint(0, 2, "Hladina je nízko!");
-
+    lcd.setCursor(0, 0);
+    lcd.print("TOO LOW");
   }
   else {
     //hladina je ok
     noTone(buzzerPin);
-    lcdPrint(0, 2, "Hladina je OK");
+    lcd.setCursor(0, 0);
+    lcd.print("OK");
   }
 
-  sendPacket(distance);
-  delay(500);
+  Serial.println(dist);
+  delay(1000);
 }
-
-void sendPacket(double distance) {
-  ps.send(reinterpret_cast<const uint8_t*>(&distance), sizeof(distance));
-}
-
 
 double distance() {
   digitalWrite(trigPin, LOW);
@@ -58,7 +56,18 @@ double distance() {
   return (duration * 0.0343) / 2;
 }
 
-void lcdPrint(int x, int y, const String &text) {
-  lcd.setCursor(x, y);
-  lcd.print(txt);
+void sendPacket(String packet) {
+  char chars = convertStringToCharArray(packet);
+  Serial.println(sizeof(chars));
+  ps.send(chars, sizeof(chars));
+}
+
+uint8_t* convertStringToCharArray(String S)
+{
+  S = S + "\n";
+  int   ArrayLength  = S.length();
+  char  CharArray[ArrayLength];
+  S.toCharArray(CharArray, ArrayLength);
+
+  return (uint8_t *)CharArray;
 }
